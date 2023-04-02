4d397adac8c592fc56fec3d4175eb36481e7b9d9
Rahul <82636553+rahulkhatri137@users.noreply.github.com>
2023-04-02 07:08:49 +0530

    debloat: more

--- debloat.sh
+++ debloat.sh
@@ -11,11 +11,11 @@ rm -rf $1/engineermode/*
 cd $1/app
 
 # Pixel
-rm -rf After* Avast* Assist* FM* Backup* Calen* Childr* Cloud* 
+rm -rf After* Avast* Atci* Assist* FM* Backup* Calen* Childr* Cloud* 
 rm -rf *FileStand com.* Decr* *Encryp* Dlna* Float* Game* Gmail2
 rm -rf Gnss* *TTS* Health* KeKeM* KeKeP* KeKeT* Keep MDM* MTKL* 
 rm -rf Maps New* OTA* OShare OVoice* Ocr* OppoAt* *Compass OppoChrome*
-rm -rf OppoD* OppoEng* OppoLog* OppoM* OppoO* OppoPhon* *Weather* *Translation*
+rm -rf OppoD* OppoEng* OppoLog* OppoM* OppoO* OppoPartner* OppoPhon* OppoQ* *Weather* *Translation*
 rm -rf Phone* Pictorial Rf* Root* SAU* *Pay Smart* SystemApp*
 rm -rf YouTube talkback
 cd $LOCALDIR
